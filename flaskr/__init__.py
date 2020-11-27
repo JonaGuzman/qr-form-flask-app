ecc4e196d5309dea71e8c0ac5265f1bbea78f3c0
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2020-11-27 13:11:41 -0500

    initial commit for qr form flask app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,9 +32,6 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from .import auth
-    app.register_blueprint(auth.bp)
-
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
