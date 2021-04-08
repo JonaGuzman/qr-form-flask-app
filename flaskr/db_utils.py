c573ae7713573474ac66ba3e242bb5c5add08664
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2021-04-08 17:29:52 -0400

    refactored, moved some methods around

--- flaskr/db_utils.py
+++ flaskr/db_utils.py
@@ -1,29 +1,4 @@
 """ Utility class for preparing SQL statements """
-# TODO: Need to test all of these
-
-
-def select_query(table_name, columns_list=None, where_dict=None):
-    cols = ""
-    if columns_list is None or columns_list == "*":
-        cols = "*"
-    else:
-        cols = get_sql_args(columns_list, str_frmt_args=False)
-
-    sel_query = "SELECT %s FROM %s" % (cols, table_name)
-    if where_dict is None:
-        return sel_query
-    else:
-        return sel_query + get_where_clause(where_dict)
-
-
-def get_id_from_tbl(table_name, where_dict, cursor):
-    cursor.execute(select_query(table_name, ['id'], where_dict))
-    id = cursor.fetchone()['id']
-    return id
-
-
-def insert_query(table_name, columns_list):
-    return "INSERT INTO %s (%s)" % (table_name, ', '.join(columns_list)) + " VALUES (%s)" % get_sql_args(columns_list)
 
 
 def get_sql_args(values_list=None, str_frmt_args=True):
@@ -40,12 +15,30 @@ def get_sql_args(values_list=None, str_frmt_args=True):
             val_args += val + ', '
     return val_args
 
+
+def select_query_str(table_name, columns=None, where=None):
+    if len(columns) > 1:
+        columns = get_sql_args(columns, str_frmt_args=False)
+    else:
+        columns = "*"
+
+    query_str = f"SELECT {columns} FROM {table_name}"
+    where_stmt = "" if where is None else get_where_clause(where)
+    print(query_str + where_stmt)
+    return query_str + where_stmt
+
+
+def insert_query_str(table_name, columns):
+    return f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({get_sql_args(columns)})"
+
+
 def frmt_arg(value):
     if isinstance(value, str):
         frmt_arg = '\'%s\''
     else:
         frmt_arg = '%d'
     return frmt_arg
+
 
 def get_where_clause(col_val_dict):
     """ Returns properly formatted WHERE clause """
@@ -54,56 +47,58 @@ def get_where_clause(col_val_dict):
     for cv in col_val_dict:
         val = col_val_dict[cv]
         if len(col_val_dict) == 1:
-            where_text += "%s = %s" % (cv,frmt_arg(val) % val)
+            where_text += "%s = %s" % (cv, frmt_arg(val) % val)
             break
         if count == len(col_val_dict) - 1:
-            where_text += "%s = %s" % (cv,frmt_arg(val) % val)
+            where_text += "%s = %s" % (cv, frmt_arg(val) % val)
             break
         else:
-            where_text += "%s = %s" % (cv,frmt_arg(val) % val) + " AND "
+            where_text += "%s = %s" % (cv, frmt_arg(val) % val) + " AND "
         count += 1
     return where_text
 
-def update_query(table_name, col_val_list, where_dict):
+
+def update_query_str(table_name, col_val_list, where):
     col_val = ""
     for cv in col_val_list:
-        col_val += "%s = ?, " % cv
+        col_val += f"{cv} = ?, "
     if col_val.endswith(', '):
         col_val = col_val.rstrip(', ')
-    return "UPDATE %s" % table_name + \
-        " SET %s" % col_val + \
-        get_where_clause(where_dict)
+    return f"UPDATE {table_name} SET {col_val +get_where_clause(where)}"
 
 
-def get_update_values_list(col_val_list):
-    """ Helper Method for getting values for SQL Cursor Execute. Prevents SQL Injection """
-    val_list = []
-    for cv in col_val_list:
-        val_list.append(cv['value'])
-    return val_list
+def get_table_id(table_name, where, cursor):
+    cursor.execute(select_query_str(table_name, ['id'], where))
+    id = cursor.fetchone()['id']
+    return id
 
-def value_exists_in_table(table_name, where_dict, cursor):
-    sql = "SELECT COUNT(*) FROM %s" % table_name + get_where_clause(where_dict)
+
+def check_if_value_in_table(table_name, where, cursor):
+    sql = f"SELECT COUNT(*) FROM {table_name + get_where_clause(where)}"
     cursor.execute(sql)
     count = cursor.fetchone()[0]
 
     if count > 0:
-        print("Existing entry in table %s" % table_name)
+        print(f"Existing entry in table {table_name}")
         return True
     else:
         return False
 
+
 def get_survey_id(email, poster_name, cursor):
-    users_id =  get_id_from_tbl('users', {'email': email}, cursor)
-    posters_id = get_id_from_tbl('posters', {'name': poster_name}, cursor)
-    up_id = get_id_from_tbl('users_posters',{'users_id': users_id, 'posters_id': posters_id}, cursor)
-    return get_id_from_tbl('surveys', {'users_posters_id': up_id}, cursor)
+    users_id = get_table_id('users', {'email': email}, cursor)
+    posters_id = get_table_id('posters', {'name': poster_name}, cursor)
+    up_id = get_table_id(
+        'users_posters', {'users_id': users_id, 'posters_id': posters_id}, cursor)
+    return get_table_id('surveys', {'users_posters_id': up_id}, cursor)
+
 
 def prep_case_seq(questions_list):
     count = 1
-    str = ""
+    case_stmt = ""
     for q in range(0, len(questions_list)):
         endline = "," if count != len(questions_list) else ""
-        str += "max(case when seq = %d then answer end) as \'%s\'%s\n" % (count, questions_list[q], endline)
+        case_stmt += "max(case when seq = %d then answer end) as \'%s\'%s\n" % (
+            count, questions_list[q], endline)
         count += 1
-    return str
+    return case_stmt
--- flaskr/survey.py
+++ flaskr/survey.py
@@ -5,8 +5,10 @@ from werkzeug.exceptions import abort
 from flaskr.db import get_db
 from flaskr import db_utils
 import collections
+import os
 import qrcode
 import pandas
+
 bp = Blueprint('survey', __name__)
 
 
@@ -19,23 +21,29 @@ def index():
 def add_poster():
     if request.method == 'POST':
         poster_name = request.form.get("poster-name")
+
         error = None
         if error is not None:
             flash(error)
         else:
             db = get_db()
             cursor = db.cursor()
-            if not db_utils.value_exists_in_table("posters", {"name": poster_name}, cursor):
-                db.execute(db_utils.insert_query(
-                    'posters', ['name', 'qr_id', 'qr_value']), (poster_name, 'qr122', ''))
-                img = qrcode.make('%s%s' % (request.url_root, poster_name))
-                poster_file_name = 'qr_code_%s.png' % poster_name.replace(
-                    " ", "_")
-                img.save('flaskr/static/images/%s' % poster_file_name)
-                return render_template('survey/add_poster.html', img_file=poster_file_name)
+
+            if not db_utils.check_if_value_in_table("posters", {"name": poster_name}, cursor):
+                db.execute(
+                    db_utils.insert_query_str(
+                        'posters', ['name', 'qr_id', 'qr_value']),
+                    (poster_name, 'qr122', '')
+                )
+
+                img_file_name = create_qrcode_img(
+                    request.url_root, poster_name)
+
+                return render_template('survey/add_poster.html', img_file=img_file_name)
             else:
                 flash("Poster name %s already exists" % poster_name)
             return redirect(url_for('survey.index'))
+
     return render_template('survey/add_poster.html', img_file=None)
 
 
@@ -62,35 +70,37 @@ def create(poster_name=None):
             db = get_db()
             cursor = db.cursor()
 
-            if not db_utils.value_exists_in_table("users", {"email": email}, cursor):
-                db.execute(db_utils.insert_query('users', ['email']), (email,))
+            if not db_utils.check_if_value_in_table("users", {"email": email}, cursor):
+                db.execute(db_utils.insert_query_str(
+                    'users', ['email']), (email,))
 
-            if not db_utils.value_exists_in_table("posters", {"name": poster_name}, cursor):
-                db.execute(db_utils.insert_query(
+            if not db_utils.check_if_value_in_table("posters", {"name": poster_name}, cursor):
+                db.execute(db_utils.insert_query_str(
                     'posters', ['name', 'qr_id', 'qr_value']), (poster_name, 'qr122', ''))
 
-            user_id = db_utils.get_id_from_tbl(
-                "users", {"email": email}, cursor)
-            poster_id = db_utils.get_id_from_tbl(
+            user_id = db_utils.get_table_id("users", {"email": email}, cursor)
+            poster_id = db_utils.get_table_id(
                 "posters", {"name": poster_name}, cursor)
-            if not db_utils.value_exists_in_table("users_posters", {"users_id": user_id, "posters_id": poster_id}, cursor):
-                db.execute(db_utils.insert_query("users_posters", ['users_id', 'posters_id']), [
-                    user_id, poster_id])
+            if not db_utils.check_if_value_in_table("users_posters", {"users_id": user_id, "posters_id": poster_id}, cursor):
+                db.execute(db_utils.insert_query_str("users_posters", [
+                           'users_id', 'posters_id']), [user_id, poster_id])
             else:
                 flash('%s and %s entry exist. Please update values now' %
                       (poster_name, email))
                 return redirect(url_for('survey.update', poster_name=poster_name, email=email))
-            users_posters_id = db_utils.get_id_from_tbl(
+            
+            users_posters_id = db_utils.get_table_id(
                 "users_posters", {"users_id": user_id, "posters_id": poster_id}, cursor)
 
-            if not db_utils.value_exists_in_table("surveys", {"users_posters_id": users_posters_id}, cursor):
-                db.execute(db_utils.insert_query("surveys", ['comment', 'users_posters_id']), [
-                    "", users_posters_id])
-            survey_id = db_utils.get_id_from_tbl(
+            if not db_utils.check_if_value_in_table("surveys", {"users_posters_id": users_posters_id}, cursor):
+                db.execute(db_utils.insert_query_str("surveys", ['comment', 'users_posters_id']),
+                    ["", users_posters_id])
+
+            survey_id = db_utils.get_table_id(
                 "surveys", {"users_posters_id": users_posters_id}, cursor)
 
             for q, a in question_answer_dict.items():
-                sql = db_utils.insert_query(
+                sql = db_utils.insert_query_str(
                     "answers", ["answer", "questions_id", "surveys_id"])
                 val = [a, q, survey_id]
                 db.execute(sql, val)
@@ -125,8 +135,7 @@ def update(poster_name, email=None):
             question_answer_dict = collections.OrderedDict()
             for key, val in request.form.items():
                 if key.startswith("a-"):
-                    question_answer_dict[int(
-                        key.replace('a-for-q', ''))] = int(val)
+                    question_answer_dict[int(key.replace('a-for-q', ''))] = int(val)
 
             for q, a in question_answer_dict.items():
                 sql = db_utils.update_query(
@@ -148,7 +157,7 @@ def update(poster_name, email=None):
             questions_list = []
             for s in questions:
                 questions_list.append(tuple(s))
-            if db_utils.value_exists_in_table("posters", {"name": poster_name}, db.cursor()):
+            if db_utils.check_if_value_in_table("posters", {"name": poster_name}, db.cursor()):
                 return redirect(url_for('survey.create', poster_name=poster_name, questions=questions_list))
 
         return render_template('survey/update.html', poster_name=poster_name, email=email, responses=surveys)
@@ -172,8 +181,9 @@ def get_survey(poster_name, email):
 
     return survey
 
-@bp.route('/get_all_surveys')
-def get_all_surveys():
+
+@bp.route('/view_all_surveys')
+def view_all_surveys():
     headers = ['id', 'email', 'Poster Name']
     questions = pandas.read_sql_query(
         "SELECT * FROM questions", get_db())['question'].values.tolist()
@@ -199,4 +209,15 @@ def get_all_surveys():
             from survey_view
             ) survey_view
             GROUP BY email""" % db_utils.prep_case_seq(questions), get_db())
-    return render_template('survey/get_all_surveys.html', headers=headers, surveys=surveys.values.tolist())
+    return render_template('survey/view_all_surveys.html', headers=headers, surveys=surveys.values.tolist())
+
+
+def create_qrcode_img(url, poster_name):
+    img = qrcode.make(f'{url}{poster_name}')
+    img_file_name = f'qr_code_{poster_name.replace(" ", "_")}.png'
+    img_dir = 'flaskr/static/images/'
+    if not os.path.isdir(img_dir):
+        os.mkdir(img_dir)
+    img.save(f'{img_dir}{img_file_name}')
+
+    return img_file_name
