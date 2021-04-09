046602f3c58729e46b494d6a83383ced4fa1f094
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2021-04-09 15:11:53 -0400

    Feature/refactor (#3)
    * refactored, moved some methods around
    
    * refactored, add helper methods for readability

--- flaskr/db_utils.py
+++ flaskr/db_utils.py
@@ -1,18 +1,20 @@
 """ Utility class for preparing SQL statements """
+from flaskr.db import get_db
+from werkzeug.exceptions import abort
+import pandas
 
 
 def get_sql_args(values_list=None, str_frmt_args=True):
     """ Returns string with ? for sql values execute method """
     val_args = ""
-    for i in range(len(values_list)):
-        if str_frmt_args:
-            val = '?'
-        else:
-            val = values_list[i]
-        if i == len(values_list) - 1:
+    for index, value in enumerate(values_list):
+        
+        val = '?' if str_frmt_args else value
+        if index == len(values_list) - 1:
             val_args += val
         else:
             val_args += val + ', '
+            
     return val_args
 
 
@@ -24,7 +26,6 @@ def select_query_str(table_name, columns=None, where=None):
 
     query_str = f"SELECT {columns} FROM {table_name}"
     where_stmt = "" if where is None else get_where_clause(where)
-    print(query_str + where_stmt)
     return query_str + where_stmt
 
 
@@ -32,29 +33,21 @@ def insert_query_str(table_name, columns):
     return f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({get_sql_args(columns)})"
 
 
-def frmt_arg(value):
-    if isinstance(value, str):
-        frmt_arg = '\'%s\''
-    else:
-        frmt_arg = '%d'
-    return frmt_arg
-
-
 def get_where_clause(col_val_dict):
     """ Returns properly formatted WHERE clause """
     where_text = " WHERE "
-    count = 0
-    for cv in col_val_dict:
+    
+    if len(col_val_dict) == 1:
+        column = list(col_val_dict.keys())[0]
+        where_text += f"{column} = '{col_val_dict[column]}'"
+        return where_text
+
+    for count, cv in enumerate(col_val_dict):
         val = col_val_dict[cv]
-        if len(col_val_dict) == 1:
-            where_text += "%s = %s" % (cv, frmt_arg(val) % val)
-            break
-        if count == len(col_val_dict) - 1:
-            where_text += "%s = %s" % (cv, frmt_arg(val) % val)
-            break
+        if count < len(col_val_dict) - 1:
+            where_text += f"{cv} = {val} AND "            
         else:
-            where_text += "%s = %s" % (cv, frmt_arg(val) % val) + " AND "
-        count += 1
+            where_text += f"{cv} = {val}"
     return where_text
 
 
@@ -64,16 +57,18 @@ def update_query_str(table_name, col_val_list, where):
         col_val += f"{cv} = ?, "
     if col_val.endswith(', '):
         col_val = col_val.rstrip(', ')
-    return f"UPDATE {table_name} SET {col_val +get_where_clause(where)}"
+    return f"UPDATE {table_name} SET {col_val + get_where_clause(where)}"
 
 
-def get_table_id(table_name, where, cursor):
+def get_table_id(table_name, where):
+    cursor = get_db().cursor()
     cursor.execute(select_query_str(table_name, ['id'], where))
     id = cursor.fetchone()['id']
     return id
 
 
-def check_if_value_in_table(table_name, where, cursor):
+def check_if_value_in_table(table_name, where):
+    cursor = get_db().cursor()
     sql = f"SELECT COUNT(*) FROM {table_name + get_where_clause(where)}"
     cursor.execute(sql)
     count = cursor.fetchone()[0]
@@ -85,15 +80,61 @@ def check_if_value_in_table(table_name, where, cursor):
         return False
 
 
-def get_survey_id(email, poster_name, cursor):
-    users_id = get_table_id('users', {'email': email}, cursor)
-    posters_id = get_table_id('posters', {'name': poster_name}, cursor)
+def get_survey_id(email, poster_name):
+    users_id = get_table_id('users', {'email': email})
+    posters_id = get_table_id('posters', {'name': poster_name})
     up_id = get_table_id(
-        'users_posters', {'users_id': users_id, 'posters_id': posters_id}, cursor)
-    return get_table_id('surveys', {'users_posters_id': up_id}, cursor)
+        'users_posters', {'users_id': users_id, 'posters_id': posters_id})
+    return get_table_id('surveys', {'users_posters_id': up_id})
 
 
-def prep_case_seq(questions_list):
+def get_survey(poster_name, email):
+    cursor = get_db().cursor()
+    survey = cursor.execute(
+        """SELECT s.id, q.question, a.answer, p.name, u.email, s.comment, p.qr_value
+        FROM surveys s
+        LEFT JOIN users_posters up ON up.id = users_posters_id
+        LEFT JOIN answers a ON a.surveys_id = s.id
+        LEFT JOIN questions q ON q.id = a.questions_id
+        LEFT JOIN posters p ON p.id = up.posters_id
+        LEFT JOIN users u ON u.id = up.users_id
+        WHERE p.name=? AND u.email=?
+        ORDER BY a.questions_id""",
+        (poster_name, email)
+    ).fetchall()
+    if survey is None:
+        abort(404, "Survey id {0} doesn't exist.".format(id))
+
+    return survey
+
+def run_survey_create_view_query():
+    cursor = get_db().cursor()
+    cursor.execute("""DROP VIEW IF EXISTS survey_view""")
+    cursor.execute(
+        """CREATE VIEW survey_view (id, question, answer, name, email, comment, qr_value) AS
+        SELECT s.id, q.question, a.answer, p.name, u.email, s.comment, p.qr_value
+        FROM surveys s
+        LEFT JOIN users_posters up ON up.id = users_posters_id
+        LEFT JOIN answers a ON a.surveys_id = s.id
+        LEFT JOIN questions q ON q.id = a.questions_id
+        LEFT JOIN posters p ON p.id = up.posters_id
+        LEFT JOIN users u ON u.id = up.users_id
+        ORDER BY q.id"""
+    )
+
+def pivot_all_surveys_table(questions):
+    """ Table with questions as columns """
+    pivot = pandas.read_sql_query(
+        f"""SELECT id, email, name, {frmt_case_seq(questions)}
+        FROM (
+            SELECT survey_view.*,
+            row_number() over (partition by email order by id) as seq
+            from survey_view
+            ) survey_view
+            GROUP BY email""", get_db())
+    return pivot
+
+def frmt_case_seq(questions_list):
     count = 1
     case_stmt = ""
     for q in range(0, len(questions_list)):
@@ -102,3 +143,10 @@ def prep_case_seq(questions_list):
             count, questions_list[q], endline)
         count += 1
     return case_stmt
+
+def run_insert_answers_query(items, survey_id):
+    for q, a in items:
+        sql = insert_query_str(
+            "answers", ["answer", "questions_id", "surveys_id"])
+        val = [a, q, survey_id]
+        get_db().execute(sql, val)
--- flaskr/survey.py
+++ flaskr/survey.py
@@ -1,7 +1,6 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-from werkzeug.exceptions import abort
 from flaskr.db import get_db
 from flaskr import db_utils
 import collections
@@ -27,19 +26,17 @@ def add_poster():
             flash(error)
         else:
             db = get_db()
-            cursor = db.cursor()
-
-            if not db_utils.check_if_value_in_table("posters", {"name": poster_name}, cursor):
+            
+            if not db_utils.check_if_value_in_table("posters", {"name": poster_name}):
                 db.execute(
                     db_utils.insert_query_str(
                         'posters', ['name', 'qr_id', 'qr_value']),
                     (poster_name, 'qr122', '')
                 )
 
-                img_file_name = create_qrcode_img(
-                    request.url_root, poster_name)
+                qr_img_file = create_qrcode_img(request.url_root, poster_name)
 
-                return render_template('survey/add_poster.html', img_file=img_file_name)
+                return render_template('survey/add_poster.html', img_file=qr_img_file)
             else:
                 flash("Poster name %s already exists" % poster_name)
             return redirect(url_for('survey.index'))
@@ -53,35 +50,29 @@ def add_poster():
 def create(poster_name=None):
     if poster_name is not None:
         poster_name = poster_name.replace("/", "")
+
     if request.method == 'POST':
         poster_name = request.form.get("poster-name")
         email = request.form.get("email")
 
         error = None
-        question_answer_dict = collections.OrderedDict()
-        for key, val in request.form.items():
-            if key.startswith("a-"):
-                question_answer_dict[int(
-                    key.replace('a-for-q', ''))] = int(val)
-
+        
         if error is not None:
             flash(error)
         else:
             db = get_db()
-            cursor = db.cursor()
 
-            if not db_utils.check_if_value_in_table("users", {"email": email}, cursor):
+            if not db_utils.check_if_value_in_table("users", {"email": email}):
                 db.execute(db_utils.insert_query_str(
                     'users', ['email']), (email,))
 
-            if not db_utils.check_if_value_in_table("posters", {"name": poster_name}, cursor):
+            if not db_utils.check_if_value_in_table("posters", {"name": poster_name}):
                 db.execute(db_utils.insert_query_str(
                     'posters', ['name', 'qr_id', 'qr_value']), (poster_name, 'qr122', ''))
 
-            user_id = db_utils.get_table_id("users", {"email": email}, cursor)
-            poster_id = db_utils.get_table_id(
-                "posters", {"name": poster_name}, cursor)
-            if not db_utils.check_if_value_in_table("users_posters", {"users_id": user_id, "posters_id": poster_id}, cursor):
+            user_id = db_utils.get_table_id("users", {"email": email})
+            poster_id = db_utils.get_table_id("posters", {"name": poster_name})
+            if not db_utils.check_if_value_in_table("users_posters", {"users_id": user_id, "posters_id": poster_id}):
                 db.execute(db_utils.insert_query_str("users_posters", [
                            'users_id', 'posters_id']), [user_id, poster_id])
             else:
@@ -90,20 +81,19 @@ def create(poster_name=None):
                 return redirect(url_for('survey.update', poster_name=poster_name, email=email))
             
             users_posters_id = db_utils.get_table_id(
-                "users_posters", {"users_id": user_id, "posters_id": poster_id}, cursor)
+                "users_posters", {"users_id": user_id, "posters_id": poster_id})
 
-            if not db_utils.check_if_value_in_table("surveys", {"users_posters_id": users_posters_id}, cursor):
-                db.execute(db_utils.insert_query_str("surveys", ['comment', 'users_posters_id']),
-                    ["", users_posters_id])
+            if not db_utils.check_if_value_in_table("surveys", {"users_posters_id": users_posters_id}):
+                db.execute(
+                    db_utils.insert_query_str("surveys",
+                     ['comment', 'users_posters_id']),
+                    ["", users_posters_id]
+                )
 
-            survey_id = db_utils.get_table_id(
-                "surveys", {"users_posters_id": users_posters_id}, cursor)
+            survey_id = db_utils.get_table_id("surveys", {"users_posters_id": users_posters_id})
 
-            for q, a in question_answer_dict.items():
-                sql = db_utils.insert_query_str(
-                    "answers", ["answer", "questions_id", "surveys_id"])
-                val = [a, q, survey_id]
-                db.execute(sql, val)
+            question_answer_dict = map_question_answer(request.form.items())
+            db_utils.run_insert_answers_query(question_answer_dict.items(), survey_id)
 
             db.commit()
             return redirect(url_for('survey.index'))
@@ -129,24 +119,16 @@ def update(poster_name, email=None):
             flash(error)
         else:
             db = get_db()
-            cursor = db.cursor()
 
-            survey_id = db_utils.get_survey_id(email, poster_name, cursor)
-            question_answer_dict = collections.OrderedDict()
-            for key, val in request.form.items():
-                if key.startswith("a-"):
-                    question_answer_dict[int(key.replace('a-for-q', ''))] = int(val)
-
-            for q, a in question_answer_dict.items():
-                sql = db_utils.update_query(
-                    "answers", ["answer"], {'surveys_id': survey_id, 'questions_id': q})
-                db.execute(sql, [a, ])
+            survey_id = db_utils.get_survey_id(email, poster_name)
+            
+            question_answer_dict = map_question_answer(request.form.items())
+            db_utils.run_insert_answers_query(question_answer_dict.items(), survey_id)
 
             db.commit()
             return redirect(url_for('survey.index'))
     else:
-        surveys = get_survey(poster_name, email)
-        # TODO: Pass in Question and Answer values into surveys when going to site/poster_name
+        surveys = db_utils.get_survey(poster_name, email)
         if len(surveys) == 0:
             db = get_db()
             questions = db.execute(
@@ -154,32 +136,14 @@ def update(poster_name, email=None):
                 ' FROM questions'
                 ' ORDER BY id'
             ).fetchall()
+
             questions_list = []
             for s in questions:
                 questions_list.append(tuple(s))
-            if db_utils.check_if_value_in_table("posters", {"name": poster_name}, db.cursor()):
+            if db_utils.check_if_value_in_table("posters", {"name": poster_name}):
                 return redirect(url_for('survey.create', poster_name=poster_name, questions=questions_list))
 
         return render_template('survey/update.html', poster_name=poster_name, email=email, responses=surveys)
-
-
-def get_survey(poster_name, email):
-    survey = get_db().execute(
-        """SELECT s.id, q.question, a.answer, p.name, u.email, s.comment, p.qr_value
-        FROM surveys s
-        LEFT JOIN users_posters up ON up.id = users_posters_id
-        LEFT JOIN answers a ON a.surveys_id = s.id
-        LEFT JOIN questions q ON q.id = a.questions_id
-        LEFT JOIN posters p ON p.id = up.posters_id
-        LEFT JOIN users u ON u.id = up.users_id
-        WHERE p.name=? AND u.email=?
-        ORDER BY a.questions_id""",
-        (poster_name, email)
-    ).fetchall()
-    if survey is None:
-        abort(404, "Survey id {0} doesn't exist.".format(id))
-
-    return survey
 
 
 @bp.route('/view_all_surveys')
@@ -188,36 +152,27 @@ def view_all_surveys():
     questions = pandas.read_sql_query(
         "SELECT * FROM questions", get_db())['question'].values.tolist()
     headers.extend(questions)
+    
+    db_utils.run_survey_create_view_query()
+    surveys = db_utils.pivot_all_surveys_table(questions)
 
-    get_db().execute("""DROP VIEW IF EXISTS survey_view""")
-    survey_view = get_db().execute(
-        """CREATE VIEW survey_view (id, question, answer, name, email, comment, qr_value) AS
-        SELECT s.id, q.question, a.answer, p.name, u.email, s.comment, p.qr_value
-        FROM surveys s
-        LEFT JOIN users_posters up ON up.id = users_posters_id
-        LEFT JOIN answers a ON a.surveys_id = s.id
-        LEFT JOIN questions q ON q.id = a.questions_id
-        LEFT JOIN posters p ON p.id = up.posters_id
-        LEFT JOIN users u ON u.id = up.users_id
-        ORDER BY q.id""")
-    surveys = pandas.read_sql_query(
-        """SELECT id, email, name,
-        %s
-        FROM (
-            SELECT survey_view.*,
-            row_number() over (partition by email order by id) as seq
-            from survey_view
-            ) survey_view
-            GROUP BY email""" % db_utils.prep_case_seq(questions), get_db())
     return render_template('survey/view_all_surveys.html', headers=headers, surveys=surveys.values.tolist())
 
 
 def create_qrcode_img(url, poster_name):
     img = qrcode.make(f'{url}{poster_name}')
     img_file_name = f'qr_code_{poster_name.replace(" ", "_")}.png'
+    
     img_dir = 'flaskr/static/images/'
     if not os.path.isdir(img_dir):
         os.mkdir(img_dir)
     img.save(f'{img_dir}{img_file_name}')
 
     return img_file_name
+
+def map_question_answer(items):
+    question_answer_dict = collections.OrderedDict()
+    for key, val in items:
+        if key.startswith("a-"):
+            question_answer_dict[int(key.replace('a-for-q', ''))] = int(val)
+    return question_answer_dict
